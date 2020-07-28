9c1bb3c93802635c45953c0b3857707f53d6cc5d
Mschkin <christianbild1@web.de>
2020-07-28 22:53:22 +0200

    solved the strange error, waiting on stackoverflow

--- test.py
+++ test.py
@@ -4,68 +4,46 @@ import cv2
 from copy import deepcopy
 import cProfile
 from scipy.signal import convolve
+from library import expit
 
 
-def back_filter_old(oldback, weight):
-    newback = np.zeros(np.shape(oldback)[:-3]+(np.shape(weight)[3], np.shape(weight)[1] +
-                                               np.shape(oldback)[-2] - 1, np.shape(weight)[2] + np.shape(oldback)[-1] - 1))
-    print(np.shape(newback))
+a=np.random.rand(3)
+e1=np.einsum('i',a)
+e2=np.einsum(a,[0])
+print([0]==list(np.array([0])))
+k=list(np.array([0]))
+e3=np.einsum(a,k)
+print(np.allclose(e1,e2))
+
+
+def back_pooling(oldback, propagation_value, dimensions):
+    # use reshape for sli and then einsum for multiply
+    newback = np.zeros(
+        np.shape(propagation_value) + np.shape(oldback)[3:])
     for i, _ in np.ndenumerate(newback):
-        newback[i] = sum([oldback[i[:-3]+(g0, g1, g2)] * weight[g0, i[-2] - g1, i[-1] - g2, i[-3]]
-                          for g0 in range(np.shape(weight)[0])
-                          for g1 in range(max(0, i[-2] - np.shape(weight)[1] + 1), min(np.shape(oldback)[-2], i[-2] + 1))
-                          for g2 in range(max(0, i[-1] - np.shape(weight)[2] + 1), min(np.shape(oldback)[-1], i[-1] + 1))])
+        sli = propagation_value[i[0] // dimensions[0] * dimensions[0]:i[0] // dimensions[0] * dimensions[0] + dimensions[0],
+                                i[1] // dimensions[1] * dimensions[1]:i[1] // dimensions[1] * dimensions[1] + dimensions[1],
+                                i[2] // dimensions[2] * dimensions[2]:i[2] // dimensions[2] * dimensions[2] + dimensions[2]]
+        newback[i] = oldback[(i[0] // dimensions[0], i[1] // dimensions[1], i[2] // dimensions[2]) + i[3:]] * (
+            (i[0] % dimensions[0], i[1] % dimensions[1], i[2] % dimensions[2]) == np.where(sli == np.max(sli)))
     return newback
 
 
-def back_filter(oldback, weight):
-    s = np.array(np.shape(oldback))
-    s[-2:] += [2*(np.shape(weight)[1]-1), 2*(np.shape(weight)[2]-1)]
-    bigold = np.zeros(s)
-    bigold[..., np.shape(weight)[1]-1:-(np.shape(weight)[1]-1),
-           np.shape(weight)[2]-1:-(np.shape(weight)[2]-1)] = oldback
-    r = []
-    for i in range(np.shape(weight)[3]):
-        r.append(convolve(bigold, np.reshape(
-            weight[::-1, :, :, i], (1,)*(len(oldback.shape)-3)+np.shape(weight)[:3]), mode='valid'))
-    return np.concatenate(tuple(r), axis=-3)
-
-def back_filter3(oldback, weight):
-    s = np.array(np.shape(oldback))
-    s[-2:] += [2*(np.shape(weight)[1]-1), 2*(np.shape(weight)[2]-1)]
-    bigold = np.zeros(s)
-    bigold[..., np.shape(weight)[1]-1:-(np.shape(weight)[1]-1),
-           np.shape(weight)[2]-1:-(np.shape(weight)[2]-1)] = oldback
-    newback = np.zeros(np.shape(oldback)[:-3]+(np.shape(weight)[3], np.shape(weight)[1] +
-                                               np.shape(oldback)[-2] - 1, np.shape(weight)[2] + np.shape(oldback)[-1] - 1))
-    for i in range(np.shape(weight)[3]):
-        newback[..., i, :, :] = convolve(bigold, np.reshape(
-            weight[::-1, :, :, i], (1,)*(len(oldback.shape)-3)+np.shape(weight)[:3]), mode='valid')[...,0,:,:]
-    return newback
+def new_pool(oldback, propagation_value, dimensions):
+    s = np.shape(propagation_value)
+    sli_pro = np.reshape(propagation_value, (s[0]//dimensions[0], dimensions[0],
+                                             s[1]//dimensions[1], dimensions[1], 
+                                             s[2]//dimensions[2], dimensions[2])+s[3:])
+    max_sli=np.max(sli_pro,axis=(1,3,5))==np.einsum('ijklmn...->jlnikm...',sli_pro)
+    print(list(range(len(s)+3)),list(range(3,len(np.shape(oldback))+3)),[3,0,4,1,5,2]+list(range(6,len(np.shape(oldback))+3)))
+    assert list(range(3,len(np.shape(oldback))+3))==list(np.arange(len(np.shape(oldback)))+3)
+    return np.reshape(np.einsum(max_sli,list(range(len(s)+3)),oldback,list(range(3,len(np.shape(oldback))+3)),[3,0,4,1,5,2]+list(range(6,len(np.shape(oldback))+3))),s[:3]+np.shape(oldback)[3:])
+    return np.reshape(np.einsum('jlnikm,ikm...->ijklmn...',max_sli,oldback),s+np.shape(oldback)[3:])
 
 
-oldback = np.random.rand(4, 4, 7, 16, 16)
-weight = np.random.rand(7, 3, 3, 2)
-
-
-k = back_filter(oldback, weight)
-m = back_filter_old(oldback, weight)
-l = back_filter3(oldback, weight)
-print(np.shape(m), np.shape(k), np.shape(l))
-print(np.allclose(l, m,k))
-
-
-"""
-a = np.random.rand(10, 10)
-b = np.random.rand(3, 3)
-c = convolve(a, b)
-an = np.zeros((14, 14))
-an[2:12, 2:12] = a
-r = np.zeros((12, 12))
-for i in range(2, 14):
-    for j in range(2, 14):
-        r[i-2, j-2] = sum([an[i-k1, j-k2]*b[k1, k2]
-                          for k1 in range(3) for k2 in range(3)])
-
-print(np.allclose(r, c))
-"""
+oldback = np.random.rand(2, 8, 8,5,2)
+propagation_value = np.random.rand(6, 16, 16)
+dimensions=(3,2,2)
+r = new_pool(oldback, propagation_value,dimensions)
+b = back_pooling(oldback, propagation_value,dimensions)
+print(np.allclose(r, b))
