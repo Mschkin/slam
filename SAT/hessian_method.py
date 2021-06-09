da47f9b2f49092ff300c24a025d04aa2bb041bc0
Markus Noeth <markusnoeth@protonmail.com>
2021-06-09 22:52:51 +0200

    finished python integrator for one special case.

--- SAT/hessian_method.py
+++ SAT/hessian_method.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-cnf="-1 -2 7 0
+cnf="""-1 -2 7 0
 -1 -3 7 0
 -2 -3 7 0
 1 2 3 0
@@ -40,27 +40,28 @@ cnf="-1 -2 7 0
 -13 -14 15 0
 -10 -11 -12 0
 10 -11 -12 0
-10 11 -12 0"
+10 11 -12 0"""
 
 lines=cnf.split("\n")
 clauses=len(lines)
 p=50
 uno=[-1-1/2-1/3]
+N=12*clauses
 
 mat=np.zeros((clauses,15))
+lino=[3]*clauses
 for i,v in enumerate(lines):
     w=v.split(" ")
-    mat[i,abs(int(w[0]))]=int(w[0])/abs(int(w[0]))
-    mat[i,abs(int(w[1]))]=int(w[1])/abs(int(w[1]))
-    mat[i,abs(int(w[2]))]=int(w[2])/abs(int(w[2]))
-
-lino=[3]*clauses
+    mat[i,abs(int(w[0]))-1]=int(w[0])/abs(int(w[0]))
+    mat[i,abs(int(w[1]))-1]=int(w[1])/abs(int(w[1]))
+    mat[i,abs(int(w[2]))-1]=int(w[2])/abs(int(w[2]))
+mat/=N
 
 mat2=[]
 bino=[]
 for i in range(clauses):
     for k in range(i,clauses):
-        mat2.append(mat[i]+mat[k])
+        mat2.append((mat[i]+mat[k])/N**2)
         if i==k:
             bino.append(-0.5*3)
         else:
@@ -71,33 +72,64 @@ trin=[]
 for i in range(clauses):
     for k in range(i,clauses):
         for l in range(k,clauses):
-            mat3.append(mat[i]+mat[k]+mat[l])
+            mat3.append((mat[i]+mat[k]+mat[l])/N**3)
             if i==k:
-                if k==j:
+                if k==l:
                     trin.append(1/3)
                 else:
                     trin.append(1)
             else:
-                if k==j:
+                if k==l:
                     trin.append(1)
                 else:
                     trin.append(2)
 
-Lintegrals=np.zeros(226)
-boundary=np.log(12*clauses)/p
-def expxx(b,pro):
+Mat =[np.zeros((15))]+ list(mat)+mat2+mat3
+weights = uno+ lino+bino+trin
+Lintegrals = np.zeros(226)
+boundary = np.log(12*clauses)/p
+print(f"boundar: {boundary}")
+
+def expxx(b, pro):
     return b**2*np.exp(pro*b)/pro - 2*b*np.exp(pro*b)/pro**2 + 2*np.exp(pro*b)/pro**3
+
+def expx(b,pro):
+    return b*np.exp(pro*b)/pro - np.exp(pro*b)/pro**2
+
 for i in range(15):
+#    print(f"hallo, {i}")
     for k in range(15):
-        v=1
-        if k==i:
-            for row in mat:
-                for ind,s in enumerate(row):
-                    if s==0 and ind!=i:
-                        v*=2*boundary
-                    elif s!=0 and ind!=i:
-                        v*=(np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
-                    elif s==0 and ind==i:
-                        v*=2*boundary**3/3
-                    elif s!=0 and ind ==i:
-                        v*=expxx(boundary,p*s)-expxx(-boundary,p*s)
+        for sumind,row in enumerate(Mat):
+            v = 1
+            if k == i:
+                for ind, s in enumerate(row):
+                    if s == 0 and ind != i:
+                        v *= 2*boundary
+                    elif s != 0 and ind != i:
+                        v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
+                    elif s == 0 and ind == i:
+                        v *= 2*boundary**3/3
+                    elif s != 0 and ind == i:
+                        v *= expxx(boundary, p*s)-expxx(-boundary, p*s)
+            if k != i:
+                for ind, s in enumerate(row):
+                    if s == 0 and ind != i and ind != k:
+                        v *= 2*boundary
+                    elif s != 0 and ind != i and ind != k:
+                        v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
+                    elif s == 0 and (ind == i or ind == k):
+                        v *= 0
+                    elif s != 0 and (ind == i or ind == k):
+                        v *= expx(boundary, p*s)-expx(-boundary, p*s)
+            Lintegrals[15*i+k]+=v*weights[sumind]
+for sumind,row in enumerate(Mat):
+    v = 1
+    for ind, s in enumerate(row):
+        if s == 0:
+            v *= 2*boundary
+        elif s != 0:
+            v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
+    Lintegrals[-1]+=v*weights[sumind]
+Lintegrals/=p
+
+print(Lintegrals)
