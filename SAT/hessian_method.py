484c9e2042d227bba662a50e8ad12eb671581f47
Markus Noeth <markusnoeth@protonmail.com>
2021-06-16 21:41:35 +0200

    fixed linear order and part of quadratic order

--- SAT/hessian_method.py
+++ SAT/hessian_method.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import binom
 
 cnf="""-1 -2 7 0
 -1 -3 7 0
@@ -46,51 +47,57 @@ lines=cnf.split("\n")
 clauses=len(lines)
 p=50
 #uno=[-1-1/2-1/3]
-uno=[-1]
+uno=[-1-1/2]
 N=12*clauses
 
 mat=np.zeros((clauses,15))
 #lino=[3]*clauses
-lino = [1]*clauses
+lino = [2/N]*clauses
 for i,v in enumerate(lines):
     w=v.split(" ")
     mat[i,abs(int(w[0]))-1]=int(w[0])/abs(int(w[0]))
     mat[i,abs(int(w[1]))-1]=int(w[1])/abs(int(w[1]))
     mat[i,abs(int(w[2]))-1]=int(w[2])/abs(int(w[2]))
-mat/=N
 
 mat2=[]
 bino=[]
 for i in range(clauses):
     for k in range(i,clauses):
-        mat2.append((mat[i]+mat[k])/N)
+        mat2.append((mat[i]+mat[k]))
         if i==k:
-            bino.append(-0.5*3)
+            bino.append(-0.5/N/N)#*3 wenn mit dritter ordnung
         else:
-            bino.append(-1*3)
+            bino.append(-1/N/N)#*3 wenn mit dritter ordnung
+            
+def genmat(n,K):
+    matn=[]
+    nno=[(-1)**(n+1)/N**n/n*(binom(K+n,K)-1)/n]*clauses**n
+    for i,_ in np.ndenumerate(np.zeros((clauses,)*n)):
+        matn.append(np.sum([mat[j] for j in i]))
+
+
 
 mat3=[]
 trin=[]
 for i in range(clauses):
     for k in range(i,clauses):
         for l in range(k,clauses):
-            mat3.append((mat[i]+mat[k]+mat[l])/N**2)
+            mat3.append((mat[i]+mat[k]+mat[l]))
             if i==k:
                 if k==l:
-                    trin.append(1/3)
+                    trin.append(1/3/N**3)
                 else:
-                    trin.append(1)
+                    trin.append(1/N**3)
             else:
                 if k==l:
-                    trin.append(1)
+                    trin.append(1/N**3)
                 else:
-                    trin.append(2)
+                    trin.append(2/N**3)
 
 #Mat =[np.zeros((14))]+ list(mat)+mat2+mat3
-Mat = [np.zeros((15))] + list(mat)
-Mat = list(mat)
+Mat = [np.zeros((15))] + list(mat)+mat2
 #weights = uno+ lino+bino+trin
-weights = uno+ lino
+weights = uno+ lino+bino
 Lintegrals = np.zeros(226)
 boundary = np.log(12*clauses)/p
 print(f"boundar: {boundary}")
@@ -137,7 +144,6 @@ for sumind,row in enumerate(Mat):
     Lintegrals[-1]+=v*weights[sumind]
 Lintegrals/=p
 
-"linear term is still wrong, debugging"
 print("weights:",weights)
 print(np.reshape(Lintegrals[:-1],(15,15)))
 print(np.linalg.norm(Lintegrals))
@@ -145,3 +151,5 @@ print(np.linalg.norm(Lintegrals))
 def ToInvert():
     H=np.zeros()
 
+#print("linear term:",(boundary**11 *2**11*boundary**3/3 *2 *(2*np.sinh(boundary
+#    *p)/p)**3/12/p-boundary**17/3 *2**15 /p))
