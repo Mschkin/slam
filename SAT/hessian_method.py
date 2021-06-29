9d9c7f9d70059de4bd93becf4c861789758349c8
Markus Noeth <markusnoeth@protonmail.com>
2021-06-29 22:08:43 +0200

    removed old code

--- SAT/hessian_method.py
+++ SAT/hessian_method.py
@@ -46,29 +46,8 @@ cnf="""-1 -2 7 0
 lines=cnf.split("\n")
 clauses=len(lines)
 p=50
-uno=[-1-1/2-1/3]
-#uno=[-1-1/2]
 N=12*clauses
 
-mat=np.zeros((clauses,15))
-lino=[3/N]*clauses
-#lino = [2/N]*clauses
-for i,v in enumerate(lines):
-    w=v.split(" ")
-    mat[i,abs(int(w[0]))-1]=int(w[0])/abs(int(w[0]))
-    mat[i,abs(int(w[1]))-1]=int(w[1])/abs(int(w[1]))
-    mat[i,abs(int(w[2]))-1]=int(w[2])/abs(int(w[2]))
-
-mat2=[]
-bino=[]
-for i in range(clauses):
-    for k in range(i,clauses):
-        mat2.append((mat[i]+mat[k]))
-        if i==k:
-            bino.append(-0.5/N/N*3)#*3 wenn mit dritter ordnung
-        else:
-            bino.append(-1/N/N*3)#*3 wenn mit dritter ordnung
-            
 def genmat(n,K,N,mat,lines):
     matn=[]
     for i,_ in np.ndenumerate(np.zeros((clauses,)*n)):
@@ -80,78 +59,12 @@ def gen_constants(n,K,N):
         return [sum(-1/k for k in range(1,K+1))]
     else:
         return [(-1)**(n+1)/N**n*binom(K,n)/n]*clauses**n
-    """
-    K=2; n=1, 1/18
-    K=2; n=2, -/(36**2)*(5/2)
-    """
-
-mat3=[]
-trin=[]
-for i in range(clauses):
-    for k in range(i,clauses):
-        for l in range(k,clauses):
-            mat3.append((mat[i]+mat[k]+mat[l]))
-            if i==k:
-                if k==l:
-                    trin.append(1/3/N**3)
-                else:
-                    trin.append(1/N**3)
-            else:
-                if k==l:
-                    trin.append(1/N**3)
-                else:
-                    trin.append(2/N**3)
-
-Mat =[np.zeros((15))]+ list(mat)+mat2+mat3
-#Mat = [np.zeros((15))] + list(mat)+mat2
-weights = uno+ lino+bino+trin
-#weights = uno+ lino+bino
-Lintegrals = np.zeros(226)
-boundary = np.log(12*clauses)/p
-print(f"boundar: {boundary}")
 
 def expxx(b, pro):
     return b**2*np.exp(pro*b)/pro - 2*b*np.exp(pro*b)/pro**2 + 2*np.exp(pro*b)/pro**3
 
 def expx(b,pro):
     return b*np.exp(pro*b)/pro - np.exp(pro*b)/pro**2
-
-print("debug1",Mat)
-for i in range(15):
-#    print(f"hallo, {i}")
-    for k in range(15):
-        for sumind,row in enumerate(Mat):
-            v = 1
-            if k == i:
-                for ind, s in enumerate(row):
-                    if s == 0 and ind != i:
-                        v *= 2*boundary
-                    elif s != 0 and ind != i:
-                        v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
-                    elif s == 0 and ind == i:
-                        v *= 2*boundary**3/3
-                    elif s != 0 and ind == i:
-                        v *= expxx(boundary, p*s)-expxx(-boundary, p*s)
-            if k != i:
-                for ind, s in enumerate(row):
-                    if s == 0 and ind != i and ind != k:
-                        v *= 2*boundary
-                    elif s != 0 and ind != i and ind != k:
-                        v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
-                    elif s == 0 and (ind == i or ind == k):
-                        v *= 0
-                    elif s != 0 and (ind == i or ind == k):
-                        v *= expx(boundary, p*s)-expx(-boundary, p*s)
-            Lintegrals[15*i+k]+=v*weights[sumind]
-for sumind,row in enumerate(Mat):
-    v = 1
-    for ind, s in enumerate(row):
-        if s == 0:
-            v *= 2*boundary
-        elif s != 0:
-            v *= (np.exp(p*s*boundary)-np.exp(-p*s*boundary))/p/s
-    Lintegrals[-1]+=v*weights[sumind]
-Lintegrals/=p
 
 
 def genLintegrals(K,N,p):
@@ -203,9 +116,8 @@ def genLintegrals(K,N,p):
     Lintegrals/=p
     return Lintegrals
 
-print("weights:",weights)
 #print(np.reshape(Lintegrals[:-1],(15,15)))
-print("diff?",np.linalg.norm(Lintegrals-genLintegrals(3,N,p)))
+print("diff?",np.linalg.norm(genLintegrals(3,N,p)))
 
 
 def ToInvert():
